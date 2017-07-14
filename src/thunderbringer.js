e2b72b2e10f80c38f653e22c5bcd068cea85a000
Jonathan <jonathan.ihm@gmail.com>
2017-07-14 13:30:44 -0500

    Update thunderbringer.js (#14)
    Fixed crappy indenting

--- src/thunderbringer.js
+++ src/thunderbringer.js
@@ -2,16 +2,16 @@ var config = require('../config.js');
 const cheerio = require('cheerio');
 
 module.exports = {
-	findNav: function($root) {
-//	    Order of precedence
-//	    1. config file
-//      2. twitter bootstrap navbar class
-//      3. nav element
-//	    4. any element with a class like nav
+    findNav: function($root) {
+        // Order of precedence
+        // 1. config file
+        // 2. twitter bootstrap navbar class
+        // 3. nav element
+        // 4. any element with a class like nav
 
-//	    TODO: support multiple navigation menus
+        // TODO: support multiple navigation menus
 
-        /// use custom selector from config first
+        // use custom selector from config first
         if (config.defaults.navMain) {
             let nav = $root.find(config.defaults.navMain);
             var tmp = this.results(nav);
@@ -20,22 +20,20 @@ module.exports = {
             }
         }
 
-
-
-	    // standard twitter navbar element
+        // standard twitter navbar element
         nav = $root.find('nav.navbar');
         tmp = this.results(nav);
         if (tmp !== null) {
             return nav;
         }
-        
+
         // other nav element
         nav = $root.find('nav');
         tmp = this.results(nav);
         if (tmp !== null) {
             return nav;
         }
-        
+
         // return anything with a class like nav
         nav = $root.find("[class*=nav], [class~='nav']");
         tmp = this.results(nav);
@@ -43,7 +41,7 @@ module.exports = {
             return nav;
         }
 
-	},
+    },
     results: function(nav) {
         if (nav.length === 1) {
             return nav;
