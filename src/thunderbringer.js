fff324ef36752f024b26a5580dcb4f36d0f3c10c
Jonathan Ihm <jonathan.ihm@gmail.com>
2017-07-21 16:07:49 +0000

    Added find footer functionality to thunderbringer

--- src/thunderbringer.js
+++ src/thunderbringer.js
@@ -1,8 +1,8 @@
-var config = require('../config.js');
 const cheerio = require('cheerio');
 
 module.exports = {
-	findNav: function($root) {
+	findNav: function($root, config) {
+	    console.log(config);
 //	    Order of precedence
 //	    1. config file
 //      2. twitter bootstrap navbar class
@@ -12,15 +12,14 @@ module.exports = {
 //	    TODO: support multiple navigation menus
 
         /// use custom selector from config first
-        if (config.defaults.navMain) {
-            let nav = $root.find(config.defaults.navMain);
+        if (config && config.defaults) {
+            var nav = $root.find(config.defaults.navMain);
+            console.log(config.defaults.navMain + " " + nav.length);
             var tmp = this.results(nav);
             if (tmp !== null) {
                 return nav;
             }
         }
-
-
 
 	    // standard twitter navbar element
         nav = $root.find('nav.navbar');
@@ -44,11 +43,45 @@ module.exports = {
         }
 
 	},
+	
+	findFooter: function($root, config) {
+//	    Easier to detect presence of footer, either a footer element
+//	    or its an element with a class that contains footer
+
+//	    Order of precedence
+//	    1. config file
+//      2. footer element
+//      3. element with footer-like class
+
+        if (config && config.defaults) {
+            var footer = $root.find(config.defaults.footer);
+            var tmp = this.results(footer);
+            if (tmp !== null) {
+                return footer;
+            }
+        }
+        // standard twitter navbar element
+        footer = $root.find('footer');
+        tmp = this.results(footer);
+        if (tmp !== null) {
+            return footer;
+        }
+        
+        // return anything with a class like nav
+        nav = $root.find("[class*=footer], [class~='footer']");
+        tmp = this.results(nav);
+        if (tmp !== null) {
+            return nav;
+        }
+	    
+    },
+
+	
     results: function(nav) {
-        if (nav.length === 1) {
+        if (nav && nav.length === 1) {
             return nav;
         } else {
             return null;
         }
-    }
+    },
 };
